07881e585f3bf79e68ac5701bc0af1dae1a510e1
Robin Heggelund Hansen <glad.sand2211@heghan.org>
2022-05-27 08:35:05 +0200

    Fill out the install page.

--- src/Page/Install.elm
+++ src/Page/Install.elm
@@ -3,10 +3,10 @@ module Page.Install exposing (Data, Model, Msg, page)
 import DataSource exposing (DataSource)
 import Head
 import Head.Seo as Seo
-import Html
+import Html exposing (Html)
+import Html.Attributes as Attribute
 import Page exposing (Page, StaticPayload)
 import Pages.PageUrl exposing (PageUrl)
-import Pages.Url
 import Shared
 import Site
 import View exposing (View)
@@ -67,5 +67,54 @@ view _ _ _ =
     , body =
         [ Html.h3 []
             [ Html.text "Install" ]
+        , Html.p []
+            [ Html.text "The latest version of Gren is 0.1.0. You can find the compiler binary for your OS at "
+            , Html.a
+                [ Attribute.href "https://github.com/gren-lang/compiler/releases/tag/v0.1.0"
+                , Attribute.title "Download the v0.1.0 compiler from Github"
+                ]
+                [ Html.text "GitHub" ]
+            , Html.text "."
+            ]
+        , Html.p []
+            [ Html.text "If you're interested in trying out the latest, though unstable, compiler then you can find builds of the main branch "
+            , Html.a
+                [ Attribute.href "https://github.com/gren-lang/compiler/actions/workflows/releases.yml"
+                , Attribute.title "Download the latest compiler from Github"
+                ]
+                [ Html.text "here" ]
+            , Html.text "."
+            ]
+        , Html.p []
+            [ Html.text "Once you have the binary, you'll need to give it permission to execute and place it somewhere in your PATH. Below is an example for Mac OS." ]
+        , Html.pre []
+            [ multilineHtmlText """
+              unzip gren-macOS.zip
+              chmod +x gren
+              mv gren /usr/local/bin/
+              """
+            ]
+        , Html.p []
+            [ Html.text "You should now be able to execute the 'gren' command and see a friendly greeting." ]
+        , Html.p []
+            [ Html.text "If you don't know what to do next, check out the "
+            , Html.a
+                [ Attribute.href "/learn"
+                , Attribute.title "language guide"
+                ]
+                [ Html.text "official language guide" ]
+            , Html.text "."
+            ]
         ]
     }
+
+
+multilineHtmlText : String -> Html a
+multilineHtmlText input =
+    input
+        |> String.lines
+        |> List.map String.trim
+        |> List.filter (not << String.isEmpty)
+        |> List.map (\s -> " " ++ s)
+        |> String.join "\n"
+        |> Html.text
